301ce6ae7e42c10e226979d65d8c0d4b7301cbf4
Adriano Faria Alves <adriano.faria@outlook.com.br>
2025-01-03 10:01:38 -0300

    Enhance HomeController to include current page and clean path in model; update navigation links in templates

--- src/main/java/br/com/siswbrasil/integrator/controller/HomeController.java
+++ src/main/java/br/com/siswbrasil/integrator/controller/HomeController.java
@@ -1,18 +1,31 @@
 package br.com.siswbrasil.integrator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
+    private String getCleanPath(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        return uri.substring(contextPath.length());
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, HttpServletRequest request) {
+        model.addAttribute("currentPage", "home");
+        model.addAttribute("cleanPath", getCleanPath(request));
         return "index";
     }
 
     @GetMapping("/about")
-    public String about() {  
+    public String about(Model model, HttpServletRequest request) {
+        model.addAttribute("currentPage", "about");
+        model.addAttribute("cleanPath", getCleanPath(request));
         return "about";
     }
 }
