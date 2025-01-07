c52505efac0eeaa7f334bd5f121b96fe6818aeca
Adriano Faria Alves <adriano.faria@outlook.com.br>
2025-01-07 19:54:47 -0300

    Add CI/CD pipeline, validation dependency, and local configuration

--- src/main/java/br/com/siswbrasil/integrator/config/OpenApiConfig.java
+++ src/main/java/br/com/siswbrasil/integrator/config/OpenApiConfig.java
@@ -3,10 +3,13 @@ package br.com.siswbrasil.integrator.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
 public class OpenApiConfig {
     @Bean
     public OpenAPI usersMicroserviceOpenAPI() {
