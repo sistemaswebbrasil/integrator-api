7d8a59f534c1acfde716d1dc01050b65c78140f6
Adriano Faria Alves <adriano.faria@outlook.com.br>
2025-01-08 01:25:14 -0300

    Add Spring Security dependencies and configure OAuth2 resource server; implement security settings and JWT authentication

--- src/main/java/br/com/siswbrasil/integrator/config/OpenApiConfig.java
+++ src/main/java/br/com/siswbrasil/integrator/config/OpenApiConfig.java
@@ -5,18 +5,31 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 @OpenAPIDefinition(servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
 public class OpenApiConfig {
+
     @Bean
-    public OpenAPI usersMicroserviceOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter the access_token (not id_token) from Auth0 response")))
                 .info(new Info()
                         .title("Integrator API")
-                        .description("API Documentation")
-                        .version("1.0"));
+                        .description("Use access_token for API authentication")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearer-key"));
     }
 }
--- src/test/java/br/com/siswbrasil/integrator/controller/TaskControllerTest.java
+++ src/test/java/br/com/siswbrasil/integrator/controller/TaskControllerTest.java
@@ -1,28 +1,38 @@
 package br.com.siswbrasil.integrator.controller;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import br.com.siswbrasil.integrator.config.TestSecurityConfig;
 import br.com.siswbrasil.integrator.entity.Task;
 import br.com.siswbrasil.integrator.exception.TaskNotFoundException;
 import br.com.siswbrasil.integrator.service.TaskService;
 
 @WebMvcTest(TaskController.class)
+@ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 public class TaskControllerTest {
 
     @Autowired
