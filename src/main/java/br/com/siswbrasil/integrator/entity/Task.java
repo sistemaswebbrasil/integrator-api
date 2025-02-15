9e83b5d080379bc52433cf1904e6d86a0dd4ab3d
Adriano Faria Alves <adriano.faria@outlook.com.br>
2025-02-15 18:04:21 -0300

    Add Flyway migration and update Task entity to track createdBy and updatedBy fields

--- src/main/java/br/com/siswbrasil/integrator/entity/Task.java
+++ src/main/java/br/com/siswbrasil/integrator/entity/Task.java
@@ -2,12 +2,18 @@ package br.com.siswbrasil.integrator.entity;
 
 import java.time.LocalDateTime;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,5 +31,34 @@ public class Task {
     private LocalDateTime createdAt;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken)) {
+            updatedBy = authentication.getName();
+        } else {
+            updatedBy = "system";
+        }
+        if(createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken)) {
+            createdBy = authentication.getName();
+        } else {
+            createdBy = "system";
+        }
+    }
+
 }
 
